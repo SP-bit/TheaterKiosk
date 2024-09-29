72c7d9765dfb3d2d311612b3c5d4b9822ef1d645
Sathwik <ps.bits5@gmail.com>
2024-09-29 13:50:34 -0400

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+        String trash = "";
+        System.out.print("Enter your age: ");
+        if(in.hasNextInt())
+        {
+            age = in.nextInt();
+            in.nextLine();
+            if(!(age < 21))
+            {
+                System.out.println("You get a wrist band");
+            }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("You said your age was " + trash);
+            System.out.println("Run the program again and enter a valid age");
+
+
+        }
+
     }
 }
